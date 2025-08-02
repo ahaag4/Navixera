e1b030e0ae9ae1399c6803c2213abed2d096ccc9
Hydra hunter <118595563+ahaag4@users.noreply.github.com>
2025-08-02 12:00:08 +0530

    Update firebase.js

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,28 +1,17 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
-import { getAuth, setPersistence, browserLocalPersistence } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCn9YSO4-ksWl6JBqIcEEuLx2EJN8jMj4M",
   authDomain: "svms-c0232.firebaseapp.com",
   databaseURL: "https://svms-c0232-default-rtdb.firebaseio.com",
   projectId: "svms-c0232",
-  storageBucket: "svms-c0232.firebasestorage.app",
+  storageBucket: "svms-c0232.appspot.com",
   messagingSenderId: "359201898609",
   appId: "1:359201898609:web:893ef076207abb06471bd0"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-// Set session persistence to local
-setPersistence(auth, browserLocalPersistence).then(() => {
-  console.log("Session persistence set to local.");
-}).catch((error) => {
-  console.error("Error setting persistence:", error);
-});
-
-export { auth, db };
+export const db = getDatabase(app);
+export const auth = getAuth(app);
